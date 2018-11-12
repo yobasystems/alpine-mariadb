2b34c53b1c3032531594d0558b27db8e2dd100c5
Dominic Taylor <dominic@dominictaylor.co.uk>
2018-11-12 11:02:50 +0000

    remove pid section until further analysis, as it is a breaking change

--- alpine-mariadb-armhf/files/run.sh
+++ alpine-mariadb-armhf/files/run.sh
@@ -5,11 +5,6 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- mysqld "$@"
 fi
 
-_get_config() {
-	local conf="$1"; shift
-	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null | awk '$1 == "'"$conf"'" { print $2; exit }'
-}
-
 # execute any pre-init scripts
 for i in /scripts/pre-init.d/*sh
 do
@@ -93,15 +88,6 @@ for f in /docker-entrypoint-initdb.d/*; do
 	esac
 	echo
 done
-
-SOCKET="$(_get_config 'socket' "$@")"
-"$@" --skip-networking --socket="${SOCKET}" &
-pid="$!"
-
-if ! kill -s TERM "$pid" || ! wait "$pid"; then
-		echo >&2 'MySQL init process failed.'
-		exit 1
-fi
 
 echo
 echo 'MySQL init process done. Ready for start up.'
