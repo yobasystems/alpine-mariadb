72544f562e2f685ab08eb7b1c01e0977e447e193
Dominic Taylor <dominic@dominictaylor.co.uk>
2019-07-23 14:10:14 +0100

    add charset selection, thanks to @RussellNS (https://github.com/RussellNS) fixing https://github.com/yobasystems/alpine-mariadb/issues/30

--- alpine-mariadb-amd64/files/run.sh
+++ alpine-mariadb-amd64/files/run.sh
@@ -54,9 +54,15 @@ EOF
 
 	if [ "$MYSQL_DATABASE" != "" ]; then
 	    echo "[i] Creating database: $MYSQL_DATABASE"
-	    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+		if [ "$MYSQL_CHARSET" != "" ] && [ "$MYSQL_COLLATION" != "" ]; then
+			echo "[i] with character set [$MYSQL_CHARSET] and collation [$MYSQL_COLLATION]"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET $MYSQL_CHARSET COLLATE $MYSQL_COLLATION;" >> $tfile
+		else
+			echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+		fi
 
-	    if [ "$MYSQL_USER" != "" ]; then
+	 if [ "$MYSQL_USER" != "" ]; then
 		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
 		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
 	    fi
